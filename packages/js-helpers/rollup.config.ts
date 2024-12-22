cb7de300507dbd80299c00571a413d3295efd9f2
Лосев Денис <denislosev48@gmail.com>
2024-12-22 14:39:16 +0300

    fix repository link, added stats tools

--- packages/js-helpers/rollup.config.ts
+++ packages/js-helpers/rollup.config.ts
@@ -4,7 +4,9 @@ import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { type Plugin, defineConfig } from "rollup";
+import { bundleStats } from "rollup-plugin-bundle-stats";
 import externals from "rollup-plugin-peer-deps-external";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const extensions = [".ts", ".js"];
 
@@ -12,13 +14,30 @@ export default defineConfig({
   input: "./src/index.ts",
   output: [
     {
-      file: "./lib/esm/index.js",
+      dir: "./lib/esm",
       format: "es",
       generatedCode: "es2015",
+      preserveModules: true,
+      preserveModulesRoot: "src",
       sourcemap: true,
+      plugins: [
+        bundleStats({
+          json: true,
+          html: true,
+          baseline: true,
+          baselineFilepath: "./baseline.json",
+          outDir: "../../stats",
+          compare: true,
+        }),
+        visualizer({
+          gzipSize: true,
+          filename: "./stats/stats.html",
+          template: "flamegraph",
+        }),
+      ],
     },
     {
-      file: "./lib/cjs/index.cjs",
+      dir: "./lib/cjs",
       format: "cjs",
       generatedCode: "es2015",
       sourcemap: true,
