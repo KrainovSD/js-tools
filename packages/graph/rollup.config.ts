ae0a600a19fb51fa4e44bdb945173f2a92bb8289
Лосев Денис <denislosev48@gmail.com>
2025-01-20 16:32:50 +0300

    fix: import d3 types

--- packages/graph/rollup.config.ts
+++ packages/graph/rollup.config.ts
@@ -3,24 +3,32 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { type Plugin, defineConfig } from "rollup";
+import { dts } from "rollup-plugin-dts";
 import externals from "rollup-plugin-peer-deps-external";
 
 const DEV = process.env.NODE_ENV === "development";
 
-export default defineConfig({
-  input: "./src/index.ts",
-  output: [
-    {
-      dir: "./lib/esm",
-      format: "es",
-      sourcemap: true,
-      preserveModules: DEV,
-    },
-  ],
-  plugins: [
-    externals({ includeDependencies: !DEV }) as Plugin,
-    typescript(),
-    DEV && nodeResolve(),
-    DEV && commonjs(),
-  ],
-});
+export default defineConfig([
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        dir: "./lib/esm",
+        format: "es",
+        sourcemap: true,
+        preserveModules: DEV,
+      },
+    ],
+    plugins: [
+      externals({ includeDependencies: !DEV }) as Plugin,
+      typescript(),
+      DEV && nodeResolve(),
+      DEV && commonjs(),
+    ],
+  },
+  {
+    input: "./tmp/index.d.ts",
+    output: [{ file: "lib/esm/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+]);
