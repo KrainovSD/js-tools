107408e3cc7ec9bc7932a2a0f3edd2bffb6d9058
Лосев Денис <denislosev48@gmail.com>
2025-03-06 15:54:33 +0300

    feature/fastify-health: add callback response

--- packages/fastify-healh-controller/src/health.service.ts
+++ packages/fastify-healh-controller/src/health.service.ts
@@ -2,6 +2,10 @@ import type { FastifyReply } from "fastify";
 
 export class HealthService {
   async health(response: unknown, reply: FastifyReply) {
-    return await reply.code(200).send(response);
+    let currentResponse = response;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    if (typeof currentResponse === "function") currentResponse = currentResponse();
+
+    return await reply.code(200).send(currentResponse);
   }
 }
