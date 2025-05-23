10c8771ce11d983eee6fbf10a44a79ca6b9d0b22
Лосев Денис <denislosev48@gmail.com>
2025-05-23 03:00:57 +0300

    fix/vue-ui: types in build

--- packages/vue-ui/vite.config.ts
+++ packages/vue-ui/vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import type { InputPluginOption } from "rollup";
 import externals from "rollup-plugin-peer-deps-external";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 const PORT = 3000;
@@ -10,7 +11,15 @@ const PORT = 3000;
 const production = process.env.npm_lifecycle_event === "build:js";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    dts({
+      outDir: "lib/",
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.build.json",
+    }),
+    vueDevTools(),
+  ],
   publicDir: production ? false : undefined,
   build: {
     sourcemap: true,
