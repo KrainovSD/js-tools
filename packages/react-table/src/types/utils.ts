304741c633b7a06efe6ac9d6171885c27c8543e9
Лосев Денис <denislosev48@gmail.com>
2025-05-29 17:11:03 +0300

    fix/react-table: add more exported types

--- packages/react-table/src/types/utils.ts
+++ packages/react-table/src/types/utils.ts
@@ -1,19 +1,38 @@
 import type {
-  CellContext,
+  CellContext as CellContextLib,
   ColumnFilter as ColumnFilterLib,
   ColumnSort as ColumnSortLib,
-  HeaderContext,
+  HeaderContext as HeaderContextLib,
   PaginationState as PaginationStateLib,
-  Row,
 } from "@tanstack/react-table";
-import type { TableInterface } from "./table";
+import type { RowInterface, TableInterface } from "./table";
 
 export type TableTypesGetter<RowData extends Record<string, unknown>> = {
-  headerContext: HeaderContext<RowData, unknown>;
-  cellContext: CellContext<RowData, unknown>;
-  row: Row<RowData>;
+  headerContext: HeaderContextLib<RowData, unknown>;
+  cellContext: CellContextLib<RowData, unknown>;
+  row: RowInterface<RowData>;
   table: TableInterface<RowData>;
+  filterFn: (row: RowInterface<RowData>, columnId: string, filterValue: unknown) => boolean;
+  sortFn: (rowA: RowInterface<RowData>, rowB: RowInterface<RowData>, columnId: string) => number;
 };
+
+export type HeaderContext<RowData extends Record<string, unknown>> = HeaderContextLib<
+  RowData,
+  unknown
+>;
+export type CellContext<RowData extends Record<string, unknown>> = CellContextLib<RowData, unknown>;
+export type Row<RowData extends Record<string, unknown>> = RowInterface<RowData>;
+export type Table<RowData extends Record<string, unknown>> = TableInterface<RowData>;
+export type FilterFn<RowData extends Record<string, unknown>> = (
+  row: RowInterface<RowData>,
+  columnId: string,
+  filterValue: unknown,
+) => boolean;
+export type SortFn<RowData extends Record<string, unknown>> = (
+  rowA: RowInterface<RowData>,
+  rowB: RowInterface<RowData>,
+  columnId: string,
+) => number;
 
 export type ColumnSort = ColumnSortLib;
 export type ColumnFilter = ColumnFilterLib;
