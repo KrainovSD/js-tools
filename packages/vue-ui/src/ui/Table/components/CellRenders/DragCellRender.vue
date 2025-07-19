6a654be92fc93c77dd231a26159bf677bcff79c2
Лосев Денис <denislosev48@gmail.com>
2025-07-19 14:25:01 +0300

    feat/vue-ui: added dark theme

--- packages/vue-ui/src/ui/Table/components/CellRenders/DragCellRender.vue
+++ packages/vue-ui/src/ui/Table/components/CellRenders/DragCellRender.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts" generic="RowData extends DefaultRow">
-  import { VDragOutlined } from "@krainovsd/vue-icons";
+  import { VHolderOutlined } from "@krainovsd/vue-icons";
   import { computed } from "vue";
+  import Button from "../../../Button.vue";
   import { checkCellVisible } from "../../lib/check-cell-visible";
   import type { CellRenderProps, DefaultRow } from "../../types";
 
@@ -18,12 +19,15 @@
 </script>
 
 <template>
-  <div
-    v-if="visible"
-    class="ksd-table__cell-drag ksd-table-row-drag-handle"
-    :class="{ drag: draggable, forbidden: !draggable }"
-  >
-    <VDragOutlined />
+  <div v-if="visible" class="ksd-table__cell-drag">
+    <Button
+      type="text"
+      size="small"
+      class="ksd-table-row-drag-handle"
+      :class="{ drag: draggable, forbidden: !draggable }"
+    >
+      <VHolderOutlined />
+    </Button>
   </div>
 </template>
 
@@ -32,7 +36,9 @@
     & > svg {
       font-size: var(--ksd-font-size-lg);
     }
+  }
 
+  button.ksd-table-row-drag-handle {
     &.drag {
       cursor: move;
     }
