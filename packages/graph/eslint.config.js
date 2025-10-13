7127aa85bcf252915ec443f3b8201cf41f0061be
Лосев Денис <denislosev48@gmail.com>
2025-10-13 16:10:41 +0300

    chore/graph: fix linter

--- packages/graph/eslint.config.js
+++ packages/graph/eslint.config.js
@@ -3,12 +3,20 @@ import plugins from "@krainovsd/presets/eslint";
 export default [
   ...plugins.presets.vue,
   {
-    ignores: ["tmp/", "node_modules/", ".turbo/", "dist/", "storybook-static/"],
+    ignores: [
+      "tmp/",
+      "node_modules/",
+      "lib/",
+      "src/typescript/",
+      ".turbo/",
+      "dist/",
+      "storybook-static/",
+    ],
   },
   {
     rules: {
       camelcase: "off",
-      "max-params": ["error", 4],
+      "max-params": "off",
     },
   },
 ];
